81d8d98b3e8c4d2d43181fee73143551e3975f4e
Faith Oluwadamilare Oyewole <devobi97@gmail.com>
2026-07-01 05:53:52 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,21 @@
-import tailwindcss from '@tailwindcss/vite'
-import { devtools } from '@tanstack/devtools-vite'
+import tailwindcss from '@tailwindcss/vite';
+import { devtools } from '@tanstack/devtools-vite';
 
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-
-import viteReact from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';1
+import { nitro } from "nitro/vite";
+import viteReact from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
   plugins: [devtools(), tailwindcss(), tanstackStart(), viteReact()],
-})
+  vite: {
+    plugins: [
+      nitro({
+        preset: "vercel",
+      }),
+    ],
+  },
+});
 
-export default config
+export default config;
