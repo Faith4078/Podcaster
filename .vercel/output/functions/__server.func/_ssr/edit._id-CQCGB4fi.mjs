c2e7c40080295f67bd7775e430cbbff63a205717
Faith Oluwadamilare Oyewole <devobi97@gmail.com>
2026-07-04 20:15:23 +0100

    Stop tracking .vercel build output

--- .vercel/output/functions/__server.func/_ssr/edit._id-CQCGB4fi.mjs
+++
@@ -1,6 +0,0 @@
-import { m as createFileRoute, p as lazyRouteComponent } from "../_libs/@tanstack/react-router+[...].mjs";
-//#region node_modules/.nitro/vite/services/ssr/assets/edit._id-CQCGB4fi.js
-var $$splitComponentImporter = () => import("./edit._id-DDVtftdC.mjs");
-var Route = createFileRoute("/edit/$id")({ component: lazyRouteComponent($$splitComponentImporter, "component") });
-//#endregion
-export { Route as t };
--- .vercel/output/static/assets/_authenticated-D0kXBJ75.js
+++
@@ -1,1 +0,0 @@
-import{r as e,t}from"./jsx-runtime-DGeXAQPT.js";import{_ as n}from"./index-ghFZdd2i.js";var r=e(t()),i=()=>(0,r.jsx)(n,{});export{i as component};
